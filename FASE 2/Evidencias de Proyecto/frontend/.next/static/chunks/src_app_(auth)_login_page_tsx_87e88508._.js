f44cb4d0694468d3274b9ea793e3913288ae0e9b
Jona238 <jon.morales@duocuc.cl>
2025-11-14 00:32:07 -0300

    chore: limpia archivos generados y actualiza .gitignore

--- FASE 2/Evidencias de Proyecto/frontend/.next/static/chunks/src_app_(auth)_login_page_tsx_87e88508._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_3ead3e92._.js",
-  "static/chunks/src_app_(auth)_login_styles_713bd991.css"
-],
-    source: "dynamic"
-});
--- FASE 2/Evidencias de Proyecto/frontend/.next/static/chunks/src_app_layout_tsx_0a548d63._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__3f5ae287._.css",
-  "static/chunks/_331e748e._.js"
-],
-    source: "dynamic"
-});
